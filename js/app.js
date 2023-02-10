7e792717f1418d07b7ed70bac31e22cc29e759b0
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-09 20:03:06 -0800

    accept y/n and yes/no answers

--- js/app.js
+++ js/app.js
@@ -12,43 +12,53 @@ if ( result ){
 }
 
 let q1 = prompt( 'Does Zeegii like coding?' );
-if ( q1.toLowerCase()==='yes'.toLowerCase() ){
+if ( q1.toLowerCase()==='yes'.toLowerCase() || q1.toLowerCase() === 'y'.toLowerCase()){
   alert('great answer!');
   //console.log('great answer!');
-} else {
+} else if ( q1.toLowerCase()==='no'.toLowerCase() || q1.toLowerCase() === 'n'.toLowerCase() ) {
   alert( 'I do like coding!!!' );
+} else {
+  alert ( 'Please insert valid answer.' );
 }
 
 let q2 = prompt( 'Do I use fork often?' );
-if ( q2.toLowerCase()==='no'.toLowerCase() ){
+if ( q2.toLowerCase()==='yes'.toLowerCase() || q2.toLowerCase() === 'y'.toLowerCase() ){
 //console.log('I love spoons');
   alert( 'I love spoons' );
-} else {
+} else if ( q2.toLowerCase()==='no'.toLowerCase() || q2.toLowerCase() === 'n'.toLowerCase() ){
   alert( 'Nahhh..... very very rarely...' );
+} else {
+  alert ( 'Please insert valid answer.' );
 }
 
 let q3 = prompt('Do I want to swtich my career?');
-if (q3.toLowerCase()==='yes'.toLowerCase()){
+if ( q3.toLowerCase()==='yes'.toLowerCase() || q3.toLowerCase() === 'y'.toLowerCase() ){
   alert('You got that right!');
   //console.log('You got that right!');
-}else {
+}else if ( q3.toLowerCase()==='no'.toLowerCase() || q3.toLowerCase() === 'n'.toLowerCase() ){
   alert('I love medicine, though I really want to try software development now.');
+} else {
+  alert ( 'Please insert valid answer.' );
 }
 
 let q4 = prompt('Have I considered to be a photographer?');
-if (q4.toLowerCase()==='yes'.toLowerCase()){
+if ( q4.toLowerCase()==='yes'.toLowerCase() || q4.toLowerCase() === 'y'.toLowerCase() ){
   alert('Yes.. My other passion');
   //console.log('Yes.. My other passion');
-}else {
+} else if ( q4.toLowerCase()==='no'.toLowerCase() || q4.toLowerCase() === 'n'.toLowerCase() ){
   alert('I have, I really have..');
+} else {
+  alert ( 'Please insert valid answer.' );
 }
 
 let q5 = prompt('Ok.. the last question... \n Am I in the school now?');
-if (q5.toLowerCase()==='yes'.toLowerCase()){
+if ( q5.toLowerCase()==='yes'.toLowerCase() || q5.toLowerCase() === 'y'.toLowerCase() ){
   alert('CODE FELLOWS!!!');
   //console.log('CODE FELLOWS!!!');
-}else {
+} else if ( q5.toLowerCase()==='no'.toLowerCase() || q5.toLowerCase() === 'n'.toLowerCase() ){
   alert(':(');
+} else {
+  alert ( 'Please insert valid answer.' );
 }
 
 let bye = `Thanks for answering all the questions ${name}!`;
