452a5f62216d397e6aad5be27bec3d2e97e8a601
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-09 19:13:49 -0800

    pass js test

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
 'use strict';
 
 let welcome = 'Welcome to my page';
-alert(welcome);
-let name = prompt('What is your name?');
-alert(`Very nice to meet you ${name} !`);
-let result = confirm('If you liked my webpage, can you answer some questions for me?');
-if (result){
-  alert('Great! Let\'s start!');
+alert( welcome );
+let name = prompt( 'What is your name?' );
+alert( `Very nice to meet you ${name} !` );
+let result = confirm( 'If you liked my webpage, can you answer some questions for me?' );
+if ( result ){
+  alert( 'Great! Let\'s start!' );
 } else {
-  alert('Oh... I have to get your permission to continue.');
+  alert( 'Oh... I have to get your permission to continue.' );
 }
 
-let q1 = prompt('Does Zeegii like coding?');
-if (q1.toLowerCase()==='yes'.toLowerCase()){
-  alert('great answer!'); 
+let q1 = prompt( 'Does Zeegii like coding?' );
+if ( q1.toLowerCase()==='yes'.toLowerCase() ){
+  alert('great answer!');
   //console.log('great answer!');
-}else {
-  alert('I do like coding!!!');
+} else {
+  alert( 'I do like coding!!!' );
 }
 
-let q2 = prompt('Do I use fork often?');
-if (q2.toLowerCase()==='no'.toLowerCase()){
+let q2 = prompt( 'Do I use fork often?' );
+if ( q2.toLowerCase()==='no'.toLowerCase() ){
 //console.log('I love spoons');
-  alert('I love spoons');
+  alert( 'I love spoons' );
 } else {
-  alert('Nahhh..... very very rarely...');
+  alert( 'Nahhh..... very very rarely...' );
 }
 
 let q3 = prompt('Do I want to swtich my career?');
 if (q3.toLowerCase()==='yes'.toLowerCase()){
-  alert('You got that right!'); 
+  alert('You got that right!');
   //console.log('You got that right!');
 }else {
   alert('I love medicine, though I really want to try software development now.');
@@ -43,9 +43,9 @@ if (q4.toLowerCase()==='yes'.toLowerCase()){
   alert('I have, I really have..');
 }
 
-let q5 = prompt('Ok.. the last question... \n Am I in the school now?'); 
+let q5 = prompt('Ok.. the last question... \n Am I in the school now?');
 if (q5.toLowerCase()==='yes'.toLowerCase()){
-  alert('CODE FELLOWS!!!'); 
+  alert('CODE FELLOWS!!!');
   //console.log('CODE FELLOWS!!!');
 }else {
   alert(':(');
