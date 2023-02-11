43915642cb870fc75745b6c4bd2d7a97010c9e2c
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-11 09:25:21 -0800

    got rid off reduntdant toLowercase

--- js/app.js
+++ js/app.js
@@ -11,51 +11,51 @@ if ( result ){
   alert( 'Oh... I have to get your permission to continue.' );
 }
 
-let q1 = prompt( 'Does Zeegii like coding?' );
-if ( q1.toLowerCase()==='yes'.toLowerCase() || q1.toLowerCase() === 'y'.toLowerCase()){
+let q1 = prompt( 'Does Zeegii like coding?' ).toLowerCase();
+if ( q1 ==='yes'|| q1 === 'y'){
   alert('great answer!');
   //console.log('great answer!');
-} else if ( q1.toLowerCase()==='no'.toLowerCase() || q1.toLowerCase() === 'n'.toLowerCase() ) {
+} else if ( q1==='no' || q1 === 'n' ) {
   alert( 'I do like coding!!!' );
 } else {
   alert ( 'Please insert valid answer.' );
 }
 
-let q2 = prompt( 'Do I use fork often?' );
-if ( q2.toLowerCase()==='yes'.toLowerCase() || q2.toLowerCase() === 'y'.toLowerCase() ){
+let q2 = prompt( 'Do I use fork often?' ).toLowerCase();
+if ( q2 ==='yes' || q2 === 'y'){
 //console.log('I love spoons');
   alert( 'I love spoons' );
-} else if ( q2.toLowerCase()==='no'.toLowerCase() || q2.toLowerCase() === 'n'.toLowerCase() ){
+} else if ( q2 ==='no' || q2 === 'n' ){
   alert( 'Nahhh..... very very rarely...' );
 } else {
   alert ( 'Please insert valid answer.' );
 }
 
-let q3 = prompt('Do I want to swtich my career?');
-if ( q3.toLowerCase()==='yes'.toLowerCase() || q3.toLowerCase() === 'y'.toLowerCase() ){
+let q3 = prompt('Do I want to swtich my career?').toLowerCase();
+if ( q3 ==='yes' || q3 === 'y' ){
   alert('You got that right!');
   //console.log('You got that right!');
-}else if ( q3.toLowerCase()==='no'.toLowerCase() || q3.toLowerCase() === 'n'.toLowerCase() ){
+}else if ( q3 ==='no' || q3 === 'n' ){
   alert('I love medicine, though I really want to try software development now.');
 } else {
   alert ( 'Please insert valid answer.' );
 }
 
-let q4 = prompt('Have I considered to be a photographer?');
-if ( q4.toLowerCase()==='yes'.toLowerCase() || q4.toLowerCase() === 'y'.toLowerCase() ){
+let q4 = prompt('Have I considered to be a photographer?').toLowerCase();
+if ( q4 ==='yes'|| q4 === 'y' ){
   alert('Yes.. My other passion');
   //console.log('Yes.. My other passion');
-} else if ( q4.toLowerCase()==='no'.toLowerCase() || q4.toLowerCase() === 'n'.toLowerCase() ){
+} else if ( q4 ==='no' || q4 === 'n' ){
   alert('I have, I really have..');
 } else {
   alert ( 'Please insert valid answer.' );
 }
 
-let q5 = prompt('Ok.. the last question... \n Am I in the school now?');
-if ( q5.toLowerCase()==='yes'.toLowerCase() || q5.toLowerCase() === 'y'.toLowerCase() ){
+let q5 = prompt('Ok.. the last question... \n Am I in the school now?').toLowerCase();
+if ( q5 ==='yes' || q5 === 'y'){
   alert('CODE FELLOWS!!!');
   //console.log('CODE FELLOWS!!!');
-} else if ( q5.toLowerCase()==='no'.toLowerCase() || q5.toLowerCase() === 'n'.toLowerCase() ){
+} else if ( q5 ==='no' || q5 === 'n' ){
   alert(':(');
 } else {
   alert ( 'Please insert valid answer.' );
