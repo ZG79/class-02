8cee8cd256c42e917a1f919facfcb5db196d2d99
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-11 16:56:57 -0800

    add new questions and info

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## This is a webpage about Zeegii Ulziibaatar
 
+### About the website
+
+The users will have to answer 7 questions to see the whole website. Depending on the questions the users will get few opportunities to guess the correct answer.
+
 ![Lighthouse Accessibility Score](/img/Accessibility_score.png)
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+let score =0;
 
 let welcome = 'Welcome to my page';
 alert( welcome );
@@ -14,6 +15,7 @@ if ( result ){
 let q1 = prompt( 'Does Zeegii like coding?' ).toLowerCase();
 if ( q1 ==='yes'|| q1 === 'y'){
   alert('great answer!');
+  score+=1;
   //console.log('great answer!');
 } else if ( q1==='no' || q1 === 'n' ) {
   alert( 'I do like coding!!!' );
@@ -24,9 +26,10 @@ if ( q1 ==='yes'|| q1 === 'y'){
 let q2 = prompt( 'Do I use fork often?' ).toLowerCase();
 if ( q2 ==='yes' || q2 === 'y'){
 //console.log('I love spoons');
-  alert( 'I love spoons' );
+  alert( 'Nah...I love spoons' );
+  score+=1;
 } else if ( q2 ==='no' || q2 === 'n' ){
-  alert( 'Nahhh..... very very rarely...' );
+  alert( 'Correct!.... very very rarely I use fork...' );
 } else {
   alert ( 'Please insert valid answer.' );
 }
@@ -34,6 +37,7 @@ if ( q2 ==='yes' || q2 === 'y'){
 let q3 = prompt('Do I want to swtich my career?').toLowerCase();
 if ( q3 ==='yes' || q3 === 'y' ){
   alert('You got that right!');
+  score+=1;
   //console.log('You got that right!');
 }else if ( q3 ==='no' || q3 === 'n' ){
   alert('I love medicine, though I really want to try software development now.');
@@ -44,6 +48,7 @@ if ( q3 ==='yes' || q3 === 'y' ){
 let q4 = prompt('Have I considered to be a photographer?').toLowerCase();
 if ( q4 ==='yes'|| q4 === 'y' ){
   alert('Yes.. My other passion');
+  score+=1;
   //console.log('Yes.. My other passion');
 } else if ( q4 ==='no' || q4 === 'n' ){
   alert('I have, I really have..');
@@ -51,9 +56,10 @@ if ( q4 ==='yes'|| q4 === 'y' ){
   alert ( 'Please insert valid answer.' );
 }
 
-let q5 = prompt('Ok.. the last question... \n Am I in the school now?').toLowerCase();
+let q5 = prompt('Ok.. the next question... \n Am I in the school now?').toLowerCase();
 if ( q5 ==='yes' || q5 === 'y'){
   alert('CODE FELLOWS!!!');
+  score+=1;
   //console.log('CODE FELLOWS!!!');
 } else if ( q5 ==='no' || q5 === 'n' ){
   alert(':(');
@@ -61,5 +67,50 @@ if ( q5 ==='yes' || q5 === 'y'){
   alert ( 'Please insert valid answer.' );
 }
 
-let bye = `Thanks for answering all the questions ${name}!`;
+let randomNum = Math.floor(Math.random()*10) + 1;
+//console.log(randomNum);
+let q6 = prompt('Please guess the number that I have in mind');
+//create another variable to store the number of tries.
+let numberOfGuesses = 0;
+while (numberOfGuesses<4){
+  numberOfGuesses+=1;
+  if (q6 > randomNum){
+    q6 = prompt('Too high, go down a little');
+  } else if (q6 < randomNum) {
+    q6 = prompt ('Too low go up a little');
+  } else if ( Number(q6) === randomNum){
+    score+=1;
+    alert('I knew You were a mind reader!');
+    break;
+  }
+}
+alert(`The correct answer was ${randomNum}`);
+//console.log(`The correct answer was ${randomNum}`);
+alert(`Your total score is ${score}.`);
+
+let photoEquipments = ['lens','battery','tripod','flashlight'];
+
+let q7 = prompt('Can you name a photographic equipments that I use often?').toLowerCase();
+let numOfAttempts = 0;
+while (numOfAttempts<4){
+  numOfAttempts+=1;
+  let found = false;
+  for (let i =0; i<photoEquipments.length; i++){
+    if (q7 === photoEquipments[i].toLowerCase()){
+      found = true;
+      break;
+    }
+  }
+  if(found){
+    alert('You got this!');
+    score+=1;
+    break;
+  } else if (numOfAttempts<4){
+    q7 = prompt('Can you try one more time?').toLowerCase();
+  }
+}
+
+alert(`The correct answers were ${photoEquipments}`);
+//console.log(score);
+let bye = `Thanks for answering all the questions ${name}, your total score is ${score}!`;
 alert(bye);
